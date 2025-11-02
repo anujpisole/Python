a2424e227c3baee63fc629b16ad50c0319dac74b
anujpisole <164508188+anujpisole@users.noreply.github.com>
2025-11-02 19:04:24 +0530

    Add files via upload

---
+++ programmiz_python.py
@@ -0,0 +1,156 @@
+# Python Program to Print Hello world!
+# print("Hello World")
+
+
+# a , b = 10 , 20
+# print(a+b)
+
+# a = 4
+# print(a**0.5)
+
+# b = 10
+# h=5
+# print(0.5*b*h)
+
+# a = 2
+# b = 10
+# c = 4
+# s1 = (-b+((b**2-4*a*c)**0.5))/2*a
+# s2 = (-b-((b**2-4*a*c)**0.5))/2*a
+# print(s1,s2)
+
+# import random
+# print(random.randint(0,10000000000))
+
+# km = 2
+# print(km * 0.621)
+
+# cel = float(input("Enter the temp in degree celsius"))
+# print("Temp in farehenite",(cel + 32))
+
+# num = int(input("Enter a number"))
+# if num > 0:
+#     print("Positive")
+# elif num < 0:
+#     print("Negaative")
+# else:
+#     print("Zero")
+
+# year = int(input("Enter the year"))
+# if year % 400 == 0 & year % 100 == 0:
+#     print("This is leap year")
+# elif year % 400 == 0 & year % 100 != 0:
+#     print("This is a leap year")
+# else:
+#     print("Non leap year")
+
+# a , b,c = 4,2,1
+
+# if a > b & b > c:
+#     print("a is greatest")
+# elif b > c:
+#     print("b is greatest")
+# else:
+#     print("c is greatest")
+
+# num = 7
+# flag = False
+# if num == 0 & num == 1:
+#     print("not prime ")
+# elif num > 1:
+#     for i in range(2,num):
+#         if(num % i == 0):
+#             flag = True
+#             break
+    
+#     if flag:
+#         print("not prime")
+#     else:
+#         print("prime")
+
+
+# for i in range(10,20):
+#     if i == 1 & i == 0:
+#         print("no prime")
+#     elif i > 1:
+#         for j in range(2,i):
+#             if i % j == 0:
+#                 break
+#         else:
+#             print(i)
+
+# fact = 1
+# for i in range(1,6):
+#     fact = fact * i
+    
+# print(fact)
+
+# for i in range(1,11):
+#     mul = 2 * i
+#     print("2 x",i,"=",mul)
+
+# n = 10
+# a = 0
+# b = 1
+# count = 0
+# while count < n :
+#     c = a+b
+#     print(a)
+#     a = b
+#     b = c
+#     count += 1
+
+# num = 157
+# order = len(str(num))
+# sum = 0
+# temp = num
+# while temp > 0:
+#     digit = temp % 10
+#     sum = sum + digit**order
+#     temp //= 10
+
+# if sum == num:
+#     print("Armstrong")
+# else:
+#     print("Not")
+
+# l = 100
+# h = 2000
+# for i in range(l,h+1):
+#     order = len(str(i))
+#     sum = 0
+#     temp = i
+#     while temp > 0:
+#         digit = temp % 10
+#         sum = sum + digit**order
+#         temp //= 10
+
+#     if i == sum:
+#         print(sum)
+
+# n = 10
+# sum = 0
+# for i in range(11):
+#     sum = sum + i
+# print(sum)
+
+# num = 2
+# for i in range(5):
+#     print("2 raised to power",i,"is",num**i)
+
+# num = 100
+# for i in range(1,101):
+#     if num % i == 0:
+#         print(i)
+
+
+
+
+
+
+
+
+
+
+
+    
